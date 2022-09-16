14cd6a974b9938d4bfaf7c964946b63bc493c41b
Nemanja Blagojevic <nemanja@darwindigital.com>
2022-09-16 23:43:02 +0200

    Load all data

--- theme/front-page.php
+++ theme/front-page.php
@@ -1,9 +1,20 @@
 <?php get_header(); ?>
 
 <main>
-  <?php require('template-parts/pizzas.php'); ?>
+  <?php require('template-parts/jela/pizzas.php'); ?>
   <?php require('template-parts/free-delivery.php'); ?>
-  <?php require('template-parts/salads.php'); ?>
+  <?php require('template-parts/jela/sendvici.php'); ?>
+  <?php require('template-parts/jela/slane-palacinke.php'); ?>
+  <?php require('template-parts/jela/slatke-palacinke.php'); ?>
+  <?php require('template-parts/jela/smoothie.php'); ?>
+  <?php require('template-parts/jela/piletina.php'); ?>
+  <?php require('template-parts/jela/pohovano.php'); ?>
+  <?php require('template-parts/jela/salads.php'); ?>
+  <?php require('template-parts/jela/tortilje.php'); ?>
+  <?php require('template-parts/jela/hrono.php'); ?>
+  <?php require('template-parts/jela/omleti.php'); ?>
+  <?php require('template-parts/jela/komplet-lepinje.php'); ?>
+  <?php require('template-parts/jela/poslastice.php'); ?>
 </main>
 
 <?php get_footer(); ?>
---
+++ theme/template-parts/jela/tortilje.php
@@ -0,0 +1,33 @@
+<section class="products" data-module="OPA">
+  <div class="container">
+    <div class="title title--with-icon">
+      <h2 class="title__header">Tortilje</h2>
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pizza.svg" alt="" class="title__icon">
+    </div>
+    <div>
+      <?php 
+        $products_IDs = new WP_Query( array(
+          'post_type' => 'product',
+          'posts_per_page' => -1,
+          'product_cat' => 'tortilje'
+        ));
+        while ($products_IDs->have_posts() ) : $products_IDs->the_post();
+        global $product;
+      ?>
+
+      <div class="product">
+          <div class="product__info">
+            <div class="product__name"><?php the_title(); ?></div>
+            <div class="product__ingredients">
+              <?php the_content(); ?>
+            </div>
+            <div class="product__price"><?php echo $product->get_price_html(); ?>rsd</div>
+          </div>
+          <div class="product__image-wrapper">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pizza.png" alt="" class="product__image">
+          </div>
+      </div>
+      <?php endwhile; ?>
+    </div>
+  </div>
+</section>
