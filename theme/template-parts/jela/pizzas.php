935f0c1de0d13b54227527c958699da98fcc0970
Nemanja Blagojevic <nemanja@darwindigital.com>
2022-09-19 17:36:07 +0200

    prikazi sve cene za pice

--- theme/template-parts/jela/pizzas.php
+++ theme/template-parts/jela/pizzas.php
@@ -13,6 +13,7 @@
         ));
         while ($products_IDs->have_posts() ) : $products_IDs->the_post();
         global $product;
+        $current_products = $product->get_children(); // vraca id svih varijacija
       ?>
 
       <div class="product">
@@ -21,7 +22,15 @@
             <div class="product__ingredients">
               <?php the_content(); ?>
             </div>
-            <div class="product__price"><?php echo $product->get_price_html(); ?>rsd</div>
+              <div class="product__prices">
+                <?php foreach($current_products as $key=>$items): ?> 
+                  <?php echo wc_get_product($items)->get_price(); ?>
+                    <?php if ($key != count($current_products) - 1): ?>
+                        <span>/</span>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+                rsd
+              </div>
           </div>
           <div class="product__image-wrapper">
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pizza.png" alt="" class="product__image">
