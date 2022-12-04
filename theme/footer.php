f18ccf66dc6dcdd886194bd484ae030c2b62ceba
Nemanja Blagojevic <nemanja@darwindigital.com>
2022-12-04 22:54:53 +0100

    Set footer to be fixed

--- theme/footer.php
+++ theme/footer.php
@@ -2,22 +2,28 @@
 <footer class="footer">
     <div class="container">
         <div class="footer__container">
-            <div class="title title--with-icon">
-                <h2>Radno vreme</h2>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/clock.svg" alt="" class="title__icon">
-            </div>
-            <p>Ponedeljak – Petak:   09:00h – 16:45h</p>
-            <?php require('template-parts/contact.php'); ?>
-            <div class="title title--with-icon">
-                <h2>Detalji</h2>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/info.svg" alt="" class="title__icon">
+            <div>
+                <div class="title title--with-icon">
+                    <h2>Radno vreme</h2>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/clock.svg" alt="" class="title__icon">
+                </div>
+                <p>Ponedeljak - Petak: 09:00h - 16:45h</p>
             </div>
             <div>
-                <p>Besplatna dostava na teritoriji Novog Beograda</p>
-                <p>Na porudžbine preko 2000 din popust 10%</p>
-                <p>Na porudžbinu preko 1500din poklon 0,5 orange juice</p>
-                <p>Velike pogodnosti za Vaše proslave (pozovite nas da se dogovorimo)</p>
-                <p>Minimum za porudžbinu 500rsd izuzev P.C”Usce”, Bezanijska kosa, Bezanija, Blokovi 44, 45, 70a, 70, 61, 62, 63, 64 gde je minimum za dostavu 1200din.</p>
+                <?php require('template-parts/contact.php'); ?>
+            </div>
+            <div>
+                <div class="title title--with-icon">
+                    <h2>Detalji</h2>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/info.svg" alt="" class="title__icon">
+                </div>
+                <div>
+                    <p>Besplatna dostava na teritoriji Novog Beograda</p>
+                    <p>Na porudžbine preko 2000 din popust 10%</p>
+                    <p>Na porudžbinu preko 1500din poklon 0,5 orange juice</p>
+                    <p>Velike pogodnosti za Vaše proslave (pozovite nas da se dogovorimo)</p>
+                    <p>Minimum za porudžbinu 500rsd izuzev P.C”Usce”, Bezanijska kosa, Bezanija, Blokovi 44, 45, 70a, 70, 61, 62, 63, 64 gde je minimum za dostavu 1200din.</p>
+                </div>
             </div>
         </div>
         <ul class="footer__social-icons">
