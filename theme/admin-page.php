a25c82ac0ff028b61495cbaac05d630f37cf4e61
Nemanja Blagojevic <nemanja@darwindigital.com>
2023-06-16 13:57:54 +0200

    Remove styling from admin page

--- theme/admin-page.php
+++ theme/admin-page.php
@@ -5,10 +5,20 @@
 ?>
 
 
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?> Opa Klopa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body class="admin-page">
+
+<?php wp_head(); ?>
 
 <main>
-    <section class="page">
+    <section>
         <div class="container container--narrow">
             <h1>Admin</h1>
             <div id="orders" data-module="ORDERS"></div>
@@ -16,4 +26,6 @@
     </section>
 </main>
 
-<?php get_footer(); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
--- theme/functions.php
+++ theme/functions.php
@@ -1,8 +1,12 @@
 <?php
 
 function wpstart_scripts() {
+  // Check if the current page is not the 'admin-page' template
+  if (!is_page_template('admin-page.php')) {
+    // Enqueue the scripts and styles
+    wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/assets/css/style.css', [], time(), $media = 'all' );
+  }
   wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/assets/js/app.min.js', [], time(), true );
-  wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/assets/css/style.css', [], time(), $media = 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'wpstart_scripts' );
