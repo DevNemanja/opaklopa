ed317fdd61fde1cf8ec8cc7ecccd9a50e351a32d
Nemanja Blagojevic <nblagojevic@aimconsulting.com>
2025-09-27 00:27:13 +0200

    dodaj priloge za rostilj

--- theme/front-page.php
+++ theme/front-page.php
@@ -12,52 +12,35 @@
     <div data-module="ORDER"></div>
 
     <?php
+    // Define all categories to render
+    $categories = [
+      // 'pizza'              => 'Pizza',
+      // 'novo-u-ponudi',
+      // 'sendvici',
+      // 'obroci-na-meru',
+      // 'opa-klopa-obroci',
+      // 'prilozi',
+      // 'burgeri',
+      'rostilj',
+      // 'klasik',
+      // 'slane-palacinke',
+      // 'palacinke',
+      // 'smoothie',
+      // 'salate',
+      // 'tortilje',
+      // 'hrono-obroci',
+      // 'komplet-lepinje',
+      // 'poslastice',
+      // 'pica',
+    ];
 
-    $product_category = 'pizza';
-    $title = 'Pizza';
-    require('template-parts/all-products-in-category.php');
+    foreach ($categories as $slug => $title) {
+      // Support both "slug only" or "slug => title"
+      $product_category = is_int($slug) ? $title : $slug;
+      $title = is_int($slug) ? null : $title;
 
-    // require('template-parts/free-delivery.php');
-
-    $product_category = 'sendvici';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'slane-palacinke';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'palacinke';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'smoothie';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'piletina';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'pohovano';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'salate';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'tortilje';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'hrono-obroci';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'omleti-u-somunu';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'komplet-lepinje';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'poslastice';
-    require('template-parts/all-products-in-category.php');
-
-    $product_category = 'pica';
-    require('template-parts/all-products-in-category.php');
-
+      require 'template-parts/all-products-in-category.php';
+    }
     ?>
 
   </div>
--- theme/template-parts/all-products-in-category.php
+++ theme/template-parts/all-products-in-category.php
@@ -125,8 +125,157 @@ $title = $category->name; // Use the category name as the title
 
 ?>
 
-
 <section class="products">
+  <div class="container">
+    <div class="title title--with-icon">
+      <h2 class="title__header"><?php echo $title ?></h2>
+      <?php echo $icon; ?>
+    </div>
+    <div class="products__wrapper">
+      <?php
+      $products_IDs = new WP_Query([
+        'post_type'      => 'product',
+        'posts_per_page' => -1,
+        'product_cat'    => $product_category
+      ]);
+      while ($products_IDs->have_posts()) : $products_IDs->the_post();
+        global $product;
+
+        $has_variations = $product->is_type('variable');
+      ?>
+
+        <div class="product"
+          data-module="ADD_TO_CART"
+          data-id="<?php echo get_the_ID(); ?>"
+          data-name="<?php the_title(); ?>"
+          data-price="<?php echo $product->get_price(); ?>"
+          data-img-url="<?php echo get_the_post_thumbnail_url($post->ID) ?>">
+
+          <div class="product__info">
+            <div class="product__name-wrapper">
+              <div class="product__name">
+                <?php the_title(); ?>
+              </div>
+              <div class="product__price"><?php echo $product->get_price_html(); ?>rsd</div>
+            </div>
+
+            <div class="product__ingredients">
+              <?php the_content(); ?>
+              <?php if ($product->has_weight()) : ?>
+                <span><?php echo wc_format_weight($product->get_weight()); ?></span>
+              <?php endif; ?>
+            </div>
+
+            <?php if ($has_variations) : ?>
+              <div class="product__variations">
+                <?php
+                $variations = $product->get_available_variations();
+                $first_variation = true;
+
+                foreach ($variations as $variation) {
+                  $variation_id  = $variation['variation_id'];
+                  $variation_obj = wc_get_product($variation_id);
+                ?>
+                  <div class="product__variation">
+                    <input class="product__variation-input"
+                      data-price="<?php echo $variation_obj->get_price(); ?>"
+                      data-id="<?php echo $variation_id; ?>"
+                      type="radio"
+                      name="<?php the_title(); ?>"
+                      id="<?php echo $variation_obj->get_name(); ?>"
+                      <?php if ($first_variation) echo 'checked'; ?>>
+                    <label class="product__variation-label" for="<?php echo $variation_obj->get_name(); ?>">
+                      <?php echo $variation_obj->get_name(); ?> <?php echo $variation_obj->get_price_html(); ?>rsd
+                    </label>
+                  </div>
+                <?php
+                  $first_variation = false;
+                }
+                ?>
+              </div>
+            <?php endif; ?>
+
+
+
+
+            <?php
+            $cross_sell_ids = $product->get_cross_sell_ids();
+
+            if (!empty($cross_sell_ids)) {
+
+              $free_products = [];
+              $paid_products = [];
+
+              foreach ($cross_sell_ids as $cs_id) {
+                $cs_product = wc_get_product($cs_id);
+                if (!$cs_product) continue;
+
+                $products_to_add = [];
+
+                if ($cs_product->is_type('grouped')) {
+                  $child_ids = $cs_product->get_children();
+                  foreach ($child_ids as $child_id) {
+                    $child_product = wc_get_product($child_id);
+                    if ($child_product) $products_to_add[] = $child_product;
+                  }
+                } else {
+                  $products_to_add[] = $cs_product;
+                }
+
+                foreach ($products_to_add as $p) {
+                  if ($p->get_price() > 0) {
+                    $paid_products[] = $p;
+                  } else {
+                    $free_products[] = $p;
+                  }
+                }
+              }
+
+              // Spoji tako da free budu prvi
+              $all_products_sorted = array_merge($free_products, $paid_products);
+
+              // Prikaz proizvoda sa checkbox-om
+              foreach ($all_products_sorted as $prod) :
+                $price = $prod->get_price();
+                $prod_id = $prod->get_id();
+                $prod_name = esc_html($prod->get_name());
+            ?>
+                <label style="display:block; margin-bottom:5px; cursor:pointer;">
+                  <input type="checkbox" name="cross_sell_products[]" value="<?= $prod_id; ?>">
+                  <?= $prod_name; ?> (ID: <?= $prod_id; ?>)
+                  <?php if ($price > 0) : ?>
+                    <span><?= wc_price($price); ?></span>
+                  <?php endif; ?>
+                </label>
+            <?php endforeach;
+            }
+            ?>
+
+
+
+
+
+
+
+            <div class="product__cart-data">
+              <button class="add-to-cart button" <?php if ($product_category === 'rostilj') echo ('data-ima-priloge'); ?>>Dodaj u korpu</button>
+            </div>
+          </div>
+
+          <div class="product__image-wrapper">
+            <?php if (get_the_post_thumbnail_url($post->ID)) :  ?>
+              <img src="<?php echo get_the_post_thumbnail_url($post->ID) ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" class="product__image">
+            <?php endif; ?>
+          </div>
+        </div>
+      <?php endwhile; ?>
+    </div>
+  </div>
+</section>
+
+
+
+<!-- <section class="products">
   <div class="container">
     <div class="title title--with-icon">
       <h2 class="title__header"><?php echo $title ?></h2>
@@ -159,6 +308,7 @@ $title = $category->name; // Use the category name as the title
               <?php endif; ?>
             </div>
 
+
             <?php if ($has_variations) : ?>
               <div class="product__variations">
                 <?php
@@ -182,7 +332,7 @@ $title = $category->name; // Use the category name as the title
 
 
             <div class="product__cart-data">
-              <button class="add-to-cart button">Dodaj u korpu</button>
+              <button class="add-to-cart button" <?php if ($product_category === 'rostilj') echo ('data-ima-priloge'); ?>>Dodaj u korpu</button>
             </div>
           </div>
           <div class="product__image-wrapper">
@@ -194,4 +344,4 @@ $title = $category->name; // Use the category name as the title
       <?php endwhile; ?>
     </div>
   </div>
-</section>
+</section> -->
