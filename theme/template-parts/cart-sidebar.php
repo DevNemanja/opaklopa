5cb646f9dcc89444582ab8b4f0272ecdd839b636
Nemanja Blagojevic <nblagojevic@aimconsulting.com>
2025-09-29 01:26:36 +0200

    prepravi popust na ceni

--- theme/template-parts/cart-sidebar.php
+++ theme/template-parts/cart-sidebar.php
@@ -19,7 +19,7 @@
     <ul class="cart-sidebar__sides">
       <!-- Populated via JS -->
     </ul>
-    <h3 class="cart-sidebar__total-amount cart-sidebar__total-amount--discounted">Total: <strong id="cart-total">1000</strong>rsd</h3>
+    <h3 class="cart-sidebar__total-amount">Total: <strong id="cart-total">1000</strong>rsd</h3>
     <h3 class="cart-sidebar__discount-amount cart-sidebar__discount-amount--hidden">Sa popustom: <strong id="cart-discount">1000</strong>rsd</h3>
 
     <!-- 🧾 Kupon kod -->
